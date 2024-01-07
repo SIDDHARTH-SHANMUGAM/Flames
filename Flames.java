a5253204e2c7a24451d1dc7a180967d17ef9c526
SIDDHARTH-SHANMUGAM <siddharthshanmugam21@gmail.com>
2024-01-07 09:57:58 +0530

    Reductio of code

--- Flames.java
+++ Flames.java
@@ -6,126 +6,25 @@ public class Flames{
         String name1=sc.next();
         System.out.println("Enter the Second Name");
         String name2=sc.next();
-
-        name1=name1.toLowerCase();
-        name2=name2.toLowerCase();
-
-        int n=name1.length();
-        int m=name2.length();
-        int count=0;
-        String s1="",s2="";
-        boolean b=true;
-        for( int i=0; i<n; i++ )
-        {
-            b=true;
-            for(int j=0; j<m; j++)
-            {
-                if(name1.charAt(i) == name2.charAt(j))
-                {
-                    s1=s1+name1.charAt(i);
-                    b=false;
-                    break;
-                }
-            }
-            if(b)
-                count++;
-        }
-        for(int j=0; j<m; j++)
-        {
-            b=true;
-            for(int i=0; i<n; i++)
-            {
-                if(name1.charAt(i) == name2.charAt(j))
-                {
-                    s2+=name2.charAt(j);
-                    b=false;
-                    break;
-                }
-            }
-            if(b)
-                count++;
-        }
-            int n1=s1.length();
-            int n2=s2.length();
-            String s3="";
-            s3=s3+s1.charAt(0);
-            for(int i=0; i<n1; i++)
-            {
-                b=true;
-                int l = s3.length();
-                for(int j=l-1; j>=0; j--)
-                {
-                    if(s1.charAt(i) == s3.charAt(j))
-                    {
-                        b=false;
-                    }
-                }
-                if(b)
-                s3+=s1.charAt(i);
-            }
-        
-        int x=s3.length();
-        int arr1[]=new int[x];
-        int arr2[]=new int[x];
-        int c=0;
-
-        for(int i=0; i<x; i++)
-        {
-            c=0;
-            for(int j=0; j<n1; j++)
-            {
-                if(s3.charAt(i) == s1.charAt(j))
-                    c++;
-            }
-            arr1[i]=c;
-        }
-
-        for(int i=0; i<x; i++)
-        {
-            c=0;
-            for(int j=0; j<n2; j++)
-            {
-                if( s3.charAt(i) == s2.charAt(j) )
-                    c++;
-            }
-            arr2[i]=c;
-        }
-        for(int i=0; i<x; i++)
-        {
-            if(arr1[i]>arr2[i])
-                count+=arr1[i]-arr2[i];
-            else
-                count+=arr2[i]-arr1[i];
-        }
-        System.out.print(StrikeLetter.strike(count));
+        int remainingletters = getRemainingLetters(name1, name2);
+        String flames = "FLAMES";
+        System.out.println(remainingletters%flames.length()==0?'S':flames.charAt((remainingletters%flames.length())-1));
 
         sc.close();
     }
-}
-class StrikeLetter {
-    public static String strike(int count){
-        String f="FLAMES";
-        int p=0;
-        int j=6;
-        while(f.length()!=1)
+    public static int getRemainingLetters(String name1, String name2)
+    {
+        StringBuilder builder1 = new StringBuilder(name1);
+        StringBuilder builder2 = new StringBuilder(name2);
+        for(int i=0; i<name1.length(); i++)
         {
-            int np=p+count;
-            while(np>j)
-            {
-                np-=j;
-            }
-            String nf="";
-            for(int i=0; i<f.length(); i++)
-            {
-                if(i==np-1)
-                    continue;
-                else
-                    nf+=f.charAt(i);
-            }
-            f=nf;
-            j--;
-            p=np-1;
+            char c = name1.charAt(i);
+           if(builder2.indexOf(c+"")!=-1)
+           {
+                builder1.deleteCharAt(builder1.indexOf(c+""));
+                builder2.deleteCharAt(builder2.indexOf(c+""));
+           }
         }
-        return f;
+        return builder1.length()+builder2.length();
     }
 }
